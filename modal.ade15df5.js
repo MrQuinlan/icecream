ec83166936e19eeffab284454d665287735cbe40
MrQuinlan <89341731+MrQuinlan@users.noreply.github.com>
2022-05-21 07:59:18 +0000

    Deploying to gh-pages from @ MrQuinlan/icecream@7f4168f752db003ee8a2771d0aec216daad687a3 🚀

---
+++ modal.ade15df5.js
@@ -0,0 +1,150 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"RSqK":[function(require,module,exports) {
+function modal(openAttr, closeAttr, modalAttr) {
+  const refs = {
+    openModalBtn: document.querySelector(openAttr),
+    closeModalBtn: document.querySelector(closeAttr),
+    modal: document.querySelector(modalAttr)
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+}
+
+(() => {
+  modal('[data-modal-open]', '[data-modal-close]', '[data-modal]');
+  modal('[data-modal-open-menu]', '[data-modal-close-menu]', '[data-modal]');
+  modal('[data-modal-open-video]', '[data-modal-close-video]', '[data-modal-video]'); // Кнопки в Products
+
+  modal('[data-modal-open-products-1]', '[data-modal-close-products-1]', '[data-modal-products-1]');
+  modal('[data-modal-open-products-2]', '[data-modal-close-products-2]', '[data-modal-products-2]');
+  modal('[data-modal-open-products-3]', '[data-modal-close-products-3]', '[data-modal-products-3]'); //               // Кнопки в Contacts
+
+  modal('[data-modal-locations-open]', '[data-modal-locations-close]', '[data-modal-locations]');
+  modal('[data-modal-franchise-open]', '[data-modal-franchise-close]', '[data-modal-franchise]'); //               // Кнопка в About
+
+  modal('[data-modal-open-about]', '[data-modal-close-about]', '[data-modal-about]');
+})();
+},{}]},{},["RSqK"], null)
+//# sourceMappingURL=/icecream/modal.ade15df5.js.map
